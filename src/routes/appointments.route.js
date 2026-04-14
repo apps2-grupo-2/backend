07d5a6246d1dbefdc30c76921edbd7535ca99547
DamianMadinelli <dmadinelli@edu.com.ar>
2026-04-14 04:30:01 -0300

    Agrego endpoints patch confirm,cancel,check-in

--- src/routes/appointments.route.js
+++ src/routes/appointments.route.js
@@ -3,6 +3,9 @@ const { validate } = require('@/middlewares/validate.middleware');
 const { createAppointmentSchema } = require('@/schemas/create-appointment.schema');
 const { getAppointmentsSchema } = require('@/schemas/get-appointments.schema');
 const { getAppointmentByIdSchema } = require('@/schemas/get-appointment-by-id.schema');
+const { patchAppointmentByIdConfirmSchema } = require('@/schemas/patch-appointment-by-id-confirm.schema');
+const { patchAppointmentByIdCancelSchema } = require('@/schemas/patch-appointment-by-id-cancel.schema');
+const { patchAppointmentByIdCheckInSchema } = require('@/schemas/patch-appointment-by-id-check-in.schema');
 
 function AppointmentsRouter(appointmentsController) {
     const router = Router();
@@ -21,6 +24,21 @@ function AppointmentsRouter(appointmentsController) {
     router.get('/:id', 
         validate(getAppointmentByIdSchema, 'params'),
         (req, res, next) => appointmentsController.getAppointmentById(req, res, next)
+    );
+
+    router.patch('/:id/confirm', 
+        validate(patchAppointmentByIdConfirmSchema, 'params'),
+        (req, res, next) => appointmentsController.patchAppointmentByIdConfirm(req, res, next)
+    );
+
+    router.patch('/:id/cancel', 
+        validate(patchAppointmentByIdCancelSchema, 'params'),
+        (req, res, next) => appointmentsController.patchAppointmentByIdCancel(req, res, next)
+    );
+
+    router.patch('/:id/check-in', 
+        validate(patchAppointmentByIdCheckInSchema, 'params'),
+        (req, res, next) => appointmentsController.patchAppointmentByIdCheckIn(req, res, next)
     );
 
     return router;
